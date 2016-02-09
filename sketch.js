d789a0019b9b5e5901c8c59cb156720bef30d391
Cornelius <cornelius.soon@gmail.com>
2016-02-10 10:31:18 +1300

    GRID AND CLASS UP

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
 	for (var i = 1; i <=(16*16); i++ ) {
-		$('#meatcenter').append('<div id="boxes" class='+i+'></div>')
+		$('#meatcenter').append('<div id="boxes" class="'+i+' dream"></div>')
 	}
 
-$('#meatcenter').find('div').each(function(){
-	if($(this).attr('class')%16===0)
-		{$(this).next().addClass('wakka')}
-});
+	$('#meatcenter').find('div').each(function(){
+		if($(this).attr('class').replace('dream', ' ')%16===0)
+			{$(this).next().addClass('wakka')}
+	});
+
+	$('#meatcenter').children().each(function(){
+		$(this).hover(function(){
+			$(this).removeClass('dream');
+			$(this).addClass('ignite');
+		});
+	});
+
+	
 });
