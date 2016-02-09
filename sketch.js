ac5383b6d88a3f3cd90488a9e66911a671ad3f31
Cornelius <cornelius.soon@gmail.com>
2016-02-09 17:44:41 +1300

    Grid sorted

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
 	for (var i = 1; i <=(16*16); i++ ) {
-		$('#meatcenter').append('<div id="boxes" class='+i+'>'+i+'</div>')
+		$('#meatcenter').append('<div id="boxes" class='+i+'></div>')
 	}
-	
 
-
+$('#meatcenter').find('div').each(function(){
+	if($(this).attr('class')%16===0)
+		{$(this).next().addClass('wakka')}
+});
 });
